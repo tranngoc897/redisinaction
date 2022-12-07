9ba02b1e61b06b40f2ef321a4ab16d95a7a004d6
TranNgoc <tranngoc897@gmail.com>
2022-12-07 18:21:37 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * https://github.com/luin/ioredis
+ * https://docs.redis.com/latest/rs/references/client_references/client_ioredis/
+ */
+const Redis = require('ioredis');
+
+const redis = new Redis({
+    host: 'redis-12347.c1.ap-southeast-1-1.ec2.cloud.redislabs.com',
+    port: 12347,
+    password: 'sjbtZdD8Jo3mZ1SwEhSb1JWqKuuVSfuF',
+});
